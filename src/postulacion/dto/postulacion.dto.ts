f1ddd68824ebd68dc53945ca845b32acbdd49d1f
SebaMolina <sebastian.molina01@alumnos.ucn.cl>
2024-07-01 04:01:55 -0400

    termino buscar horario

--- src/postulacion/dto/postulacion.dto.ts
+++ src/postulacion/dto/postulacion.dto.ts
@@ -2,6 +2,6 @@ export class PostulacionDTO {
     readonly servicioId: string;
     readonly usuarioId: string;
     readonly mensaje: string;
-    readonly fechaSolicitada: string; // Formato 'YYYY-MM-DD'
+    readonly fechaSolicitada: Date; // Formato 'YYYY-MM-DD'
     readonly horarioSolicitado: string; // Formato 'HH:mm'
   }
--- src/postulacion/schema/postulacion.schema.ts
+++ src/postulacion/schema/postulacion.schema.ts
@@ -5,7 +5,7 @@ export const PostulacionSchema = new mongoose.Schema(
     servicioId: { type: String, required: true },
     usuarioId: { type: String, required: true },
     mensaje: { type: String, required: true },
-    fechaSolicitada: { type: String, required: true },
+    fechaSolicitada: { type: Date, required: true },
     horarioSolicitado: { type: String, required: true },
   },
   { timestamps: true },
