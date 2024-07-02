78fca22cda4d1d08f128e63791f8c1b9407f9c1f
SebaMolina <sebastian.molina01@alumnos.ucn.cl>
2024-07-02 01:13:02 -0400

    review listo + endpoint + update arreglado

--- src/common/interfaces/services.interface.ts
+++ src/common/interfaces/services.interface.ts
@@ -11,7 +11,13 @@ export interface IService extends Document {
   user_id: string;
   rating: number;
   contadorSolicitudes: number;
-  
+  reviews: IReview[];
  
 }
 
+interface IReview {
+  userId: string; // ID del usuario que deja la review
+  rating: number; // Puntuación de 1 a 5 estrellas
+  comentario: string; // Comentario opcional
+  fecha: Date; // Fecha en que se dejó la review
+}
